4bc843747f6270a552c0a2b866201ec28055b24b
Spike <xevvvw@gmail.com>
2025-11-12 13:16:51 -0800

    chore: update skills

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -43,6 +43,7 @@ const SkillsSection: React.FC = () => {
         "Git",
         "Docker",
         "REST APIs",
+        "Postman",
         "CI/CD (Travis)",
         "Agile/Scrum",
         "Linux",
